86d97bfe8db8e0add9b9307fbe47f1ced072907b
Elonge11 <47829151+Elonge11@users.noreply.github.com>
2020-06-17 18:51:38 +0200

    install mongodb

---
+++ NodeJS/database.js
@@ -0,0 +1,11 @@
+// request for mongoose
+const mongoose = require('mongoose');
+// make connection to db by moogose then we can add parameter err
+mongoose.connect('url', (err) => {
+    if(!err)
+        console.log('MongoDB connection succeeded');
+    else
+        console.log('Error in DB connection:' + JSON.stringify(err, undefined,2));
+} );
+
+module.exports = mongoose;
