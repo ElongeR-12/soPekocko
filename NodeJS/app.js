59f5640a71234e11133d981d72026a5db0239c2a
Elonge11 <47829151+Elonge11@users.noreply.github.com>
2020-06-19 00:00:39 +0200

    allow cors between server and frontend

--- NodeJS/app.js
+++ NodeJS/app.js
@@ -2,7 +2,12 @@
 const express = require('express');
 //express() method to create express app
 const app = express();
-
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+  });
 app.use((req,res)=>{
     res.json({message: 'votre requête sur application express fonctionne'})
 })
