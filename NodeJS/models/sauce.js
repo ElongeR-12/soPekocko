1323c0b6dfd55d1b00c6a04e46be724be698917d
Elonge11 <47829151+Elonge11@users.noreply.github.com>
2020-06-21 00:03:04 +0200

    re-implement sauce model

--- NodeJS/models/sauce.js
+++ NodeJS/models/sauce.js
@@ -2,11 +2,17 @@
 const mongoose = require('mongoose');
 //the model for sauce
 const sauceSchema = mongoose.Schema({
+  userId: { type: String, required: true },
   name: { type: String, required: true },
-  price: { type: Number, required: true },
+  manufacturer: { type: String, required: true },
   description: { type: String, required: true },
-  lenses: { type: [String], required: true },
-  imageUrl: { type: String, required: true }
+  mainPepper: { type: String, required: true },
+  imageUrl: { type: String, required: true },
+  heate: { type: Number, required: true },
+  likes: { type: Number, required: true },
+  dislikes: { type: Number, required: true },
+  usersLiked: { type: [String], required: true },
+  usersDisliked: { type: [String], required: true }
 });
 //export this sauce
 module.exports = mongoose.model('Sauce', sauceSchema);
