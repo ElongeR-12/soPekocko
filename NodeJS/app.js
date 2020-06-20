cb0bed3f2ffcdf9f25cc5d0733b7bccb8b37006e
Elonge11 <47829151+Elonge11@users.noreply.github.com>
2020-06-21 00:36:48 +0200

    import sauce routes and use in app

--- NodeJS/app.js
+++ NodeJS/app.js
@@ -3,16 +3,16 @@ const express = require('express');
 //express() method to create express app
 const app = express();
 const database = require('./database');
-// const Sauce = require('./models/sauce');
+const Sauce = require('./models/sauce');
 const User = require('./models/user');
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 const cors = require('cors');
 app.use(cors());
-// const saucesRoutes = require('./models/sauce');
+const saucesRoutes = require('./models/sauce');
 const userRoutes = require('./routes/user');
 
-// app.use('/api/sauces', saucesRoutes);
+app.use('/api/sauces', saucesRoutes);
 app.use('/api/auth', userRoutes);
 
 module.exports = app;
