17ec6966b57945cbae3f6751fee2f50635c4bb1b
Elonge11 <47829151+Elonge11@users.noreply.github.com>
2020-06-21 15:34:09 +0200

    implement middloware at routes

--- NodeJS/routes/sauce.js
+++ NodeJS/routes/sauce.js
@@ -2,11 +2,12 @@ const express = require('express');
 const router = express.Router();
 
 const sauceCtrl = require('../controllers/sauce');
+const auth = require('../middleware/auth');
 
-router.get('/', sauceCtrl.getAllSauses);
-router.post('/', sauceCtrl.createSauce);
-router.get('/:id', sauceCtrl.getOneSauce);
-router.put('/:id', sauceCtrl.modifySauce);
-router.delete('/:id', sauceCtrl.deleteSauce);
+router.get('/', auth, sauceCtrl.getAllSauces);
+router.post('/', auth, sauceCtrl.createSauce);
+router.get('/:id', auth, sauceCtrl.getOneSauce);
+router.put('/:id', auth, sauceCtrl.modifySauce);
+router.delete('/:id', auth, sauceCtrl.deleteSauce);
 
 module.exports = router;
