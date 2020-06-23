5eb0f80782fd453e7214d7cd956362d52e0c9153
Elonge11 <47829151+Elonge11@users.noreply.github.com>
2020-06-23 22:11:41 +0200

    correct model schema

--- NodeJS/models/sauce.js
+++ NodeJS/models/sauce.js
@@ -2,17 +2,16 @@
 const mongoose = require('mongoose');
 //the model for sauce
 const sauceSchema = mongoose.Schema({
-  userId: { type: String, required: true },
   name: { type: String, required: true },
   manufacturer: { type: String, required: true },
   description: { type: String, required: true },
   mainPepper: { type: String, required: true },
   imageUrl: { type: String, required: true },
-  heate: { type: Number, required: true },
-  likes: { type: Number, required: false },
-  dislikes: { type: Number, required: false },
-  usersLiked: { type: [String], required: false },
-  usersDisliked: { type: [String], required: false }
+  heat: { type: Number, required: true },
+  likes: { type: Number },
+  dislikes: { type: Number },
+  usersLiked: { type: [String] },
+  usersDisliked: { type: [String] }
 });
 //export this sauce
 module.exports = mongoose.model('Sauce', sauceSchema);
