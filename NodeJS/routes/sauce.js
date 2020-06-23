9de8cabb3b9bb0c8069d6a7ea6d3931b29d017f0
Elonge11 <47829151+Elonge11@users.noreply.github.com>
2020-06-23 22:02:40 +0200

    add multer middleware

--- NodeJS/routes/sauce.js
+++ NodeJS/routes/sauce.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const auth = require('../middleware/auth');
-
+const multer = require('../middleware/multer-config');
 const sauceCtrl = require('../controllers/sauce');
 
 router.get('/', sauceCtrl.getAllSauces);
-router.post('/', auth, sauceCtrl.createSauce);
+router.post('/', auth, multer, sauceCtrl.createSauce);
 router.get('/:id', sauceCtrl.getOneSauce);
 router.put('/:id', auth, sauceCtrl.modifySauce);
 router.delete('/:id', auth, sauceCtrl.deleteSauce);
