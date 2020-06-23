1fda1500a0b47379ea79615c1c40021917da973e
Elonge11 <47829151+Elonge11@users.noreply.github.com>
2020-06-23 22:07:27 +0200

    require path and manage routage

--- NodeJS/app.js
+++ NodeJS/app.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const database = require('./database');
+const path = require('path');
 const saucesRoutes = require('./routes/sauce');
 const userRoutes = require('./routes/user');
 const app = express();
 const cors = require('cors');
 app.use(cors());
 app.use(bodyParser.json());
+app.use('/images', express.static(path.join(__dirname, 'images')));
 app.use('/api/sauces', saucesRoutes);
 app.use('/api/auth', userRoutes);
-
 module.exports = app;
