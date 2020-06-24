3df1aa5083efc67834a9a0e136f27737a36f3e68
Elonge11 <47829151+Elonge11@users.noreply.github.com>
2020-06-24 11:57:25 +0200

    fix bug to show modify and delete button, add user ID in model

--- NodeJS/models/sauce.js
+++ NodeJS/models/sauce.js
@@ -2,16 +2,17 @@
 const mongoose = require('mongoose');
 //the model for sauce
 const sauceSchema = mongoose.Schema({
+  userId:{type:String, required: true},
   name: { type: String, required: true },
   manufacturer: { type: String, required: true },
   description: { type: String, required: true },
   mainPepper: { type: String, required: true },
   imageUrl: { type: String, required: true },
   heat: { type: Number, required: true },
-  likes: { type: Number },
-  dislikes: { type: Number },
-  usersLiked: { type: [String] },
-  usersDisliked: { type: [String] }
+  likes: { type: Number,  required: true },
+  dislikes: { type: Number,  required: true },
+  usersLiked: { type: [String],  required: true },
+  usersDisliked: { type: [String],  required: true }
 });
 //export this sauce
 module.exports = mongoose.model('Sauce', sauceSchema);
