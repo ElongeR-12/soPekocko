180227ea8dc2e845b8ff3eb98d929a4530fe8f48
Elonge11 <47829151+Elonge11@users.noreply.github.com>
2020-07-11 19:24:36 +0200

    update connection to secify user roles of databases

--- NodeJS/database.js
+++ NodeJS/database.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv').config();
 let db = process.env.DB_USER;
 const pwd = process.env.DB_PASS;
-mongoose.connect(`mongodb+srv://${db}:${pwd}@cluster-p-six-oc-kum0q.mongodb.net/${pwd}?retryWrites=true&w=majority`,
+let dbName = process.env.DB_NAME;
+mongoose.connect(`mongodb+srv://${db}:${pwd}@cluster-p-six-oc-kum0q.mongodb.net/${dbName}?retryWrites=true&w=majority`,
   { useNewUrlParser: true,
     useUnifiedTopology: true })
   .then(() => console.log('Connexion à MongoDB réussie !'))
